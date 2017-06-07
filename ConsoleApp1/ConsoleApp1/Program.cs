3e8bd219358dbd0f66093ba8c49840f87585c97e
Волосович Сергей <volosovich_sergey@mail.ru>
2017-06-08 00:04:27 +0300

    Расчет методом Евклида

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using NODFinder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] x;
+            string  str;
+            Class1 nf = new Class1();
+            int ans;
+
+            Console.WriteLine("Введите числа через пробел");
+            str = Console.ReadLine();
+            x=str.Split(' ');
+
+            int [] y = new int[x.Length];
+
+            for (int i=0; i<x.Length; i++)
+            {
+                y[i] = int.Parse(x[i]);
+            }
+            ans=nf.FindNOD(y[0], y[1], y[2], y[3], y[4]);
+            Console.Write("НОД ");
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(y[i] + " ");
+            }
+            Console.Write("чисел равно:" + ans);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/NODFinder/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace NODFinder
+{
+    public class Class1
+    {
+        public int FindNOD(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a >= b) a = a % b;
+                else b = b % a;
+            }
+            return a + b;
+
+        }
+        public int FindNOD(int a, int b, int c)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a >= b) a = a % b;
+                else b = b % a;
+            }
+            a = a + b;
+            while (a != 0 && c != 0)
+            {
+                if (a >= c) a = a % c;
+                else c = c % a;
+            }
+            return a + c;
+        }
+        public int FindNOD(params int[] a)
+        {
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                while (a[0] != 0 && a[i] != 0)
+                {
+                    if (a[0] >= a[i]) a[0] = a[0] % a[i];
+                    else a[i] = a[i] % a[0];
+                }
+                a[0] = a[0] + a[i];
+            }
+            return a[0];
+        }
+    }
+}
