a2cadce48cdbfee973f393fe50ae159c80f205f6
Волосович Сергей <volosovich_sergey@mail.ru>
2017-06-08 00:16:22 +0300

    Окончательный Евклида без таймера

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,16 +14,18 @@ namespace ConsoleApp1
             string  str;
             Class1 nf = new Class1();
             int ans;
+            int[] y = new int[5];
 
             Console.WriteLine("Введите числа через пробел");
             str = Console.ReadLine();
             x=str.Split(' ');
-
-            int [] y = new int[x.Length];
-
-            for (int i=0; i<x.Length; i++)
+            for (int i=0; i<5; i++)
             {
-                y[i] = int.Parse(x[i]);
+                if (x.Length > i)
+                {
+                    y[i] = int.Parse(x[i]);
+                }
+                else y[i] = 0;
             }
             ans=nf.FindNOD(y[0], y[1], y[2], y[3], y[4]);
             Console.Write("НОД ");
