0cbc8be418778700fbcedbe36ed78f3614cb54b7
Волосович Сергей <volosovich_sergey@mail.ru>
2017-06-08 17:26:07 +0300

    Таймер добавлен

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleApp1
 {
@@ -11,15 +12,18 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string[] x;
-            string  str;
+            string  str, ans, ans2;
             Class1 nf = new Class1();
-            int ans;
-            int[] y = new int[5];
-
-            Console.WriteLine("Введите числа через пробел");
-            str = Console.ReadLine();
-            x=str.Split(' ');
-            for (int i=0; i<5; i++)
+            int a;
+            int[] y;
+            do
+            {
+                Console.WriteLine("Введите числа через пробел и не менее двух");
+                str = Console.ReadLine();
+                x = str.Split(' ');
+            } while (x.Length < 2);
+            y = new int[x.Length+1];
+            for (int i=0; i<x.Length; i++)
             {
                 if (x.Length > i)
                 {
@@ -27,13 +31,46 @@ namespace ConsoleApp1
                 }
                 else y[i] = 0;
             }
-            ans=nf.FindNOD(y[0], y[1], y[2], y[3], y[4]);
+            while (true)
+            {
+                Console.WriteLine("Требуется ли подсчет времени выполнения расчетов(Да/Нет)");
+
+                string rasch = Console.ReadLine();
+
+                if (rasch == "Да")
+                {
+                    a = 1;
+                    y[x.Length] = a;
+                    break;
+                }
+                else if (rasch =="Нет")
+                {
+                    a = 0;
+                    y[x.Length] = a;
+                    break;
+                }
+            }
+                switch (x.Length)
+            {
+                case 2:
+                    ans = nf.FindNOD(y[0], y[1], a);
+                    ans2 = nf.FindNOD2(y);
+                    break;
+                case 3:
+                    ans = nf.FindNOD(y[0], y[1], y[2], a);
+                    ans2 = nf.FindNOD2(y);
+                    break;
+                default:
+                    ans = nf.FindNOD(y);
+                    ans2 = nf.FindNOD2(y);
+                    break;
+            }
             Console.Write("НОД ");
             for (int i = 0; i < x.Length; i++)
             {
                 Console.Write(y[i] + " ");
             }
-            Console.Write("чисел равно:" + ans);
+            Console.Write("чисел:\nпо методу Евклида равно " + ans+"\nпо методу Стейна равно "+ans2);
             Console.Read();
         }
     }
--- ConsoleApp1/NODFinder/Class1.cs
+++ ConsoleApp1/NODFinder/Class1.cs
@@ -1,21 +1,34 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace NODFinder
 {
     public class Class1
     {
-        public int FindNOD(int a, int b)
+        public string FindNOD(int a, int b, int c)
         {
-            while (a != 0 && b != 0)
-            {
-                if (a >= b) a = a % b;
-                else b = b % a;
-            }
-            return a + b;
+            Stopwatch sw = new Stopwatch();
+            TimeSpan ts = new TimeSpan();
 
+            sw.Start();
+            while (a != 0 && b != 0)
+                {
+                    if (a >= b) a = a % b;
+                    else b = b % a;
+                }
+            sw.Stop();
+            ts = sw.Elapsed;
+            if (c > 0)
+            {
+                return a + b + " время затраченое на выполнение метода: " + ts.TotalMilliseconds + "миллисекунд";
+            }else return a + b +"";
         }
-        public int FindNOD(int a, int b, int c)
+        public string FindNOD(int a, int b, int c, int d)
         {
+            Stopwatch sw = new Stopwatch();
+            TimeSpan ts = new TimeSpan();
+
+            sw.Start();
             while (a != 0 && b != 0)
             {
                 if (a >= b) a = a % b;
@@ -27,11 +40,21 @@ namespace NODFinder
                 if (a >= c) a = a % c;
                 else c = c % a;
             }
-            return a + c;
+            sw.Stop();
+            ts = sw.Elapsed;
+            if (d > 0)
+            {
+                return a + c + " время затраченое на выполнение метода: " + ts.TotalMilliseconds + " миллисекунд";
+            }
+            else return a + c + "";
         }
-        public int FindNOD(params int[] a)
+        public string FindNOD(params int[] d)
         {
+            int[] a = (int[])d.Clone();
+            Stopwatch sw = new Stopwatch();
+            TimeSpan ts = new TimeSpan();
 
+            sw.Start();
             for (int i = 1; i < a.Length; i++)
             {
                 while (a[0] != 0 && a[i] != 0)
@@ -41,7 +64,38 @@ namespace NODFinder
                 }
                 a[0] = a[0] + a[i];
             }
-            return a[0];
+            sw.Stop();
+            ts = sw.Elapsed;
+            if (d[d.Length-1] > 0)
+            {
+                return a[0] + " время затраченое на выполнение метода: " + ts.TotalMilliseconds + " миллисекунд";
+            }
+            else return a[0] +  "";
+        }
+        public string FindNOD2(params int[] d)
+        {
+            int[] a = (int[])d.Clone();
+            int b = 1;
+            Stopwatch sw = new Stopwatch();
+            TimeSpan ts = new TimeSpan();
+
+            sw.Start();
+            for (int i = 1; i < a.Length; i++)
+            {
+                while (a[0] != 0 && a[i] != 0)
+                {
+                    if (a[0] >= a[i]) a[0] = a[0] % a[i];
+                    else a[i] = a[i] % a[0];
+                }
+                a[0] = a[0] + a[i];
+            }
+            sw.Stop();
+            ts = sw.Elapsed;
+            if (d[d.Length - 1] > 0)
+            {
+                return a[0] + " время затраченое на выполнение метода: " + ts.TotalMilliseconds + " миллисекунд";
+            }
+            else return a[0] + "";
         }
     }
 }
