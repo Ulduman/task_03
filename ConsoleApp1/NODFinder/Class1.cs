b2bed7c1b7e19716326d3481f5f8ccddbf21200d
Волосович Сергей <volosovich_sergey@mail.ru>
2017-06-08 18:42:07 +0300

    Добавлен метод Стейна+таймер

--- ConsoleApp1/NODFinder/Class1.cs
+++ ConsoleApp1/NODFinder/Class1.cs
@@ -20,7 +20,7 @@ namespace NODFinder
             ts = sw.Elapsed;
             if (c > 0)
             {
-                return a + b + " время затраченое на выполнение метода: " + ts.TotalMilliseconds + "миллисекунд";
+                return a + b + " время затраченое на выполнение метода: " + ts.TotalMilliseconds + " миллисекунд";
             }else return a + b +"";
         }
         public string FindNOD(int a, int b, int c, int d)
@@ -55,7 +55,7 @@ namespace NODFinder
             TimeSpan ts = new TimeSpan();
 
             sw.Start();
-            for (int i = 1; i < a.Length; i++)
+            for (int i = 1; i < a.Length-1; i++)
             {
                 while (a[0] != 0 && a[i] != 0)
                 {
@@ -75,19 +75,40 @@ namespace NODFinder
         public string FindNOD2(params int[] d)
         {
             int[] a = (int[])d.Clone();
-            int b = 1;
             Stopwatch sw = new Stopwatch();
             TimeSpan ts = new TimeSpan();
 
             sw.Start();
-            for (int i = 1; i < a.Length; i++)
+            for (int i = 1; i < a.Length-1; i++)
             {
+                int b = 1;
+
                 while (a[0] != 0 && a[i] != 0)
                 {
-                    if (a[0] >= a[i]) a[0] = a[0] % a[i];
-                    else a[i] = a[i] % a[0];
+                    if (a[0] % 2 == 0 && a[i] % 2 == 0)
+                    {
+                        b = b * 2;
+                        a[0] = a[0] / 2;
+                        a[i] = a[i] / 2;
+                    } else if (a[0] % 2 == 0 && a[i] % 2 == 1)
+                    {
+                        a[0] = a[0] / 2;
+                    } else if (a[0] % 2 == 1 && a[i] % 2 == 0)
+                    {
+                        a[i] = a[i] / 2;
+                    } else if (a[0] % 2 == 1 && a[i] % 2 == 1 && a[0] >= a[i])
+                    {
+                        a[0] = a[0] - a[i];
+                    }else if(a[0]%2==1 && a[i]%2==1 && a[0] < a[i])
+                    {
+                        a[i] = a[i] - a[0];
+                    }
                 }
-                a[0] = a[0] + a[i];
+                if (a[0] == 0)
+                {
+                    a[0] = b * a[i];
+                }
+                else a[0] = b * a[0];
             }
             sw.Stop();
             ts = sw.Elapsed;
