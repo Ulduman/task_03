907852e4cf457913e41f7db16e6b11341a7a913c
Волосович Сергей <volosovich_sergey@mail.ru>
2017-06-10 14:00:59 +0300

    Добавлен тест

---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace UnitTestProject1
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void FindNOD()
+        {
+            int a = 10, b = 20, c = 1;
+            Stopwatch sw = new Stopwatch();
+            TimeSpan ts = new TimeSpan();
+
+            sw.Start();
+            while (a != 0 && b != 0)
+            {
+                if (a >= b) a = a % b;
+                else b = b % a;
+            }
+            sw.Stop();
+            ts = sw.Elapsed;
+        }
+    }
+}
